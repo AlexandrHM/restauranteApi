c9ed54e94d7b2e4aaeecc148c02617e3d42ab52d
OyasumiiAlex <b181010@unach.mx>
2024-06-18 10:14:09 -0600

    config(add): cors port config
    version 1.0.8-alpha

---
+++ src/main/java/com/website/restaurant/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.website.restaurant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/estudiantes/**") // Ruta de la API
+                .allowedOrigins("http://localhost:3000") // Origen permitido
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE"); // Métodos permitidos
+    }
+}
