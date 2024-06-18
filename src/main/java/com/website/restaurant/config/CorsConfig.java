60b67f52d005e6b6b15e606f8a0bf57fe97e0c77
OyasumiiAlex <b181010@unach.mx>
2024-06-18 10:16:57 -0600

    config(add): cors port config
    version 1.0.8-alpha

--- src/main/java/com/website/restaurant/config/CorsConfig.java
+++ src/main/java/com/website/restaurant/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/estudiantes/**") // Ruta de la API
+        registry.addMapping("/api/v1/restaurante/**") // Ruta de la API
                 .allowedOrigins("http://localhost:3000") // Origen permitido
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE"); // Métodos permitidos
     }
